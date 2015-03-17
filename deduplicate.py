4000d5c3248fcd2a2028acafcca56142b09d3644
Nick Smith <ncsmith6@wisc.edu>
2015-03-17 01:16:59 -0500

    hack to deduplicate singlemu dataset from doublemu done

--- deduplicate.py
+++ deduplicate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import pickle
+import pickle, array
 import ROOT
 import util
 
@@ -15,16 +15,38 @@ def getRunEvtTuples(chain) :
 
 def deduplicate(left_dataset, right_dataset, tuple_path) :
     left_tree = util.buildChain('datasets/%s.root' % left_dataset, tuple_path)
-    right_tree = util.buildChain('datasets/%s.root' % right_dataset, tuple_path)
-    left_set = getRunEvtTuples(singlemu)
+    left_set = getRunEvtTuples(left_tree)
     print "Built set for %s" % left_dataset
-    right_set = getRunEvtTuples(doublemu)
+
+    right_tree = util.buildChain('datasets/%s.root' % right_dataset, tuple_path)
+    right_set = getRunEvtTuples(right_tree)
     print "Built set for %s" % right_dataset
+
     dupes = left_set.intersection(right_set)
     print "Found intersection, pickling. (overlap,sum = %d,%d)" % (len(dupes), len(left_set)+len(right_set))
 
     with open('datasets/%s.duplicates.pickle' % left_dataset, 'w') as out :
         pickle.dump(dupes, out, 2)
+
+def makeDupeBranch(dataset_name, tuple_path) :
+    treefile = ROOT.TFile('datasets/%s.root' % dataset_name, 'update')
+    tree = treefile.Get(tuple_path)
+
+    duplicates = pickle.load(open('datasets/%s.duplicates.pickle' % dataset_name))
+
+    duplicate_event = array.array('i',[0])
+    branch = tree.Branch('duplicate_event', duplicate_event, 'duplicate_event/I')
+    
+    for i in range(tree.GetEntries()) :
+        tree.GetEntry(i)
+        if (tree.run, tree.evt) in duplicates :
+            duplicate_event[0] = 1
+        else :
+            duplicate_event[0] = 0
+        branch.Fill()
+
+    treefile.cd(tuple_path[:tuple_path.rfind('/')])
+    tree.Write('', ROOT.TObject.kOverwrite)
 
 if __name__ == '__main__' :
     singlemu = [
@@ -42,4 +64,5 @@ if __name__ == '__main__' :
     ]
 
     for i in range(4) :
-        deduplicate(singlemu[i], doublemu[i])
+        deduplicate(singlemu[i], doublemu[i], 'mm/Ntuple')
+        makeDupeBranch(singlemu[i], 'mm/Ntuple')
--- stackUp.py
+++ stackUp.py
@@ -46,7 +46,10 @@ def stackUp(**kwargs) :
         plotgroup = info['plotgroup']
         h = findGroupHist(plotgroup)
         hname = h.GetName()
-        kwargs['trees'][dataname].Draw(kwargs['variable']+">>+"+hname, cut)
+        singlemuhack = ''
+        if 'SingleMu' in dataname :
+            singlemuhack = ' && duplicate_event==0'
+        kwargs['trees'][dataname].Draw(kwargs['variable']+">>+"+hname, cut+singlemuhack)
         if info['type'] == 'mc' and not 'signal' in info.get('flags',[]) :
             tostack[plotgroup] = h
         elif info['type'] == 'data' :
