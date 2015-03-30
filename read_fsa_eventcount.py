789ece5aab6884d159c84c3f2f71ca48fe805570
Nick Smith <ncsmith6@wisc.edu>
2015-03-30 18:03:32 -0500

    Investigating pat/ntuple event count discrepancy

--- read_fsa_eventcount.py
+++ read_fsa_eventcount.py
@@ -4,19 +4,16 @@ import meta
 
 for name, info in meta.ZHinv_datasets.iteritems() :
     shortname = info['matching_pat'].keys()[0]
-    hevt = None
+    ntuple_total = 0
     with open('datasets/%s.ntuples.txt' % shortname) as filelist :
         for line in filelist :
             f = ROOT.TFile.Open(line.strip())
-            if hevt :
-                hevt.Add(f.Get('ee/eventCount'))
-            else :
-                hevt = f.Get('ee/eventCount').Clone()
-    ntuple_total = hevt.GetBinContent(1)
+            hevt = f.Get('ee/eventCount')
+            ntuple_total += hevt.GetBinContent(1)
+
     with open("datasets/"+shortname+".ntuple_eventcount.txt", 'w') as out :
         out.write("%d\n" % ntuple_total)
 
-    missing_events = 0
     das_nevents = info['dbs_info']['nevents']
 
     print "%9d, %9d : %s" % (ntuple_total, das_nevents, name)
