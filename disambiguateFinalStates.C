0203c339f12d16708da2010c87e189c1869e42dd
Nick Smith <ncsmith6@wisc.edu>
2015-03-30 14:44:23 -0500

    Made proof-frendly TSelector for disambituation

---
+++ disambiguateFinalStates.C
@@ -0,0 +1,65 @@
+#define disambiguateFinalStates_cxx
+
+#include "disambiguateFinalStates.h"
+
+
+void disambiguateFinalStates::Begin(TTree * /*tree*/)
+{
+}
+
+void disambiguateFinalStates::SlaveBegin(TTree * /*tree*/)
+{
+  fBestCandidateEntryList = new TEntryList("bestCandidates", "Entry List of disambiguated combinatoric candidates");
+  fOutput->Add(fBestCandidateEntryList);
+}
+
+Bool_t disambiguateFinalStates::Process(Long64_t entry)
+{
+  b_Mass->GetEntry(entry);
+  b_evt->GetEntry(entry);
+  b_run->GetEntry(entry);
+
+  if ( !fPostInit && !(run == fCurrentRun && evt == fCurrentEvt) )
+  {
+    Long64_t bestEntry = 0L;
+    Float_t lowestDiscriminant = 1e10;
+    for (size_t i=0; i<fEntriesToCompare.size(); ++i)
+    {
+      if ( lowestDiscriminant > fEntryDiscriminants[i] )
+      {
+        lowestDiscriminant = fEntryDiscriminants[i];
+        bestEntry = fEntriesToCompare[i];
+      }
+    }
+
+    fBestCandidateEntryList->Enter(bestEntry);
+
+    fEntriesToCompare.clear();
+    fEntryDiscriminants.clear();
+  }
+  else if ( fPostInit )
+  {
+    fPostInit = false;
+  }
+
+  fCurrentRun = run;
+  fCurrentEvt = evt;
+  
+  Float_t discriminant = fabs(Mass-91.);
+
+  fEntriesToCompare.push_back(entry);
+  fEntryDiscriminants.push_back(discriminant);
+
+  return kTRUE;
+}
+
+void disambiguateFinalStates::SlaveTerminate()
+{
+  fBestCandidateEntryList->OptimizeStorage();
+  // Pointer is owned by fOutput, dereference
+  fBestCandidateEntryList = nullptr;
+}
+
+void disambiguateFinalStates::Terminate()
+{
+}
---
+++ disambiguateFinalStates.h
@@ -0,0 +1,70 @@
+#ifndef disambiguateFinalStates_h
+#define disambiguateFinalStates_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TSelector.h>
+#include <TEntryList.h>
+
+class disambiguateFinalStates : public TSelector {
+public :
+  TTree          *fChain;
+
+  Float_t         Mass;
+  Int_t           evt;
+  Int_t           run;
+
+  TBranch        *b_Mass;
+  TBranch        *b_evt;
+  TBranch        *b_run;
+
+  TEntryList     *fBestCandidateEntryList;
+
+  disambiguateFinalStates(TTree * /*tree*/ =0) : fChain(0), fBestCandidateEntryList(0) { }
+  virtual ~disambiguateFinalStates() { SafeDelete(fBestCandidateEntryList); }
+  virtual Int_t   Version() const { return 2; }
+  virtual void    Begin(TTree *tree);
+  virtual void    SlaveBegin(TTree *tree);
+  virtual void    Init(TTree *tree);
+  virtual Bool_t  Notify();
+  virtual Bool_t  Process(Long64_t entry);
+  virtual Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
+  virtual void    SetOption(const char *option) { fOption = option; }
+  virtual void    SetObject(TObject *obj) { fObject = obj; }
+  virtual void    SetInputList(TList *input) { fInput = input; }
+  virtual TList  *GetOutputList() const { return fOutput; }
+  virtual void    SlaveTerminate();
+  virtual void    Terminate();
+
+  ClassDef(disambiguateFinalStates,0);
+
+private :
+  bool  fPostInit;
+  Int_t fCurrentRun;
+  Int_t fCurrentEvt;
+  std::vector<Long64_t> fEntriesToCompare;
+  std::vector<Float_t> fEntryDiscriminants;
+};
+
+#endif
+
+#ifdef disambiguateFinalStates_cxx
+void disambiguateFinalStates::Init(TTree *tree)
+{
+  if (!tree) return;
+  fChain = tree;
+
+  fChain->SetBranchAddress("Mass", &Mass, &b_Mass);
+  fChain->SetBranchAddress("evt", &evt, &b_evt);
+  fChain->SetBranchAddress("run", &run, &b_run);
+
+  fPostInit = true;
+}
+
+Bool_t disambiguateFinalStates::Notify()
+{
+  return kTRUE;
+}
+
+#endif // #ifdef disambiguateFinalStates_cxx
